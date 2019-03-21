addd31b148dc6d4dc66e3c74fa2afb26579b110d
weichangyao <wyao1@stuy.edu>
2019-03-21 12:02:01 -0400

    added other constructor and size

--- MyDeque.java
+++ MyDeque.java
@@ -5,15 +5,27 @@ public class MyDeque<E>{
   @SuppressWarnings("unchecked")
   public MyDeque(){
     data = (E[])new Object[10];
+    start = 0;
+    end = data.length;
+    size = 10;
   }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
+  public MyDeque(int initialCapacity){
+    @SuppressWarnings("unchecked")
+    data = (E[])new Object[initialCapacity];
+    start = 0;
+    end = data.length;
+    int size = initialCapacity;
+  }
+
+  public int size(){
+    return size;
+  }
+  /*
   public String toString(){ }
   public void addFirst(E element){ }
   public void addLast(E element){ }
   public E removeFirst(){ }
   public E removeLast(){ }
   public E getFirst(){ }
-  public E getLast(){ }
+  public E getLast(){ }*/
 }
-
