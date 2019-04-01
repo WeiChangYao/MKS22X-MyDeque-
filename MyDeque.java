ee9b7b0c2964875b26c8c08ffc67e1145a345214
wyao1 <wyao1@stuy.edu>
2019-04-01 02:21:29 -0400

    fixed size

--- MyDeque.java
+++ MyDeque.java
@@ -1,21 +1,23 @@
 import java.util.*;
 public class MyDeque<E>{
   private E[] data;
-  private int size, start, end;
+  private int size2, size, start, end;
 
   @SuppressWarnings("unchecked")
   public MyDeque(){
     data = (E[])new Object[10];
     start = -1;
     end = 0;
-    size = 10;
+    size2 = 10;
+    size = 0;
   }
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
     start = -1;
     end = 0;
-    int size = initialCapacity;
+    size2 = initialCapacity;
+    size = 0;
   }
 /*
   public class Calculator{
@@ -32,7 +34,7 @@ public class MyDeque<E>{
         
  //////////////////////////////
   public boolean isFull(){ 
-    return ((start == 0 && end == size-1)|| 
+    return ((start == 0 && end == size2-1)|| 
             start == end+1); 
   } 
  /////////////////////////////
@@ -41,20 +43,23 @@ public class MyDeque<E>{
   } 
  //////////////////////////////
 
-
   public int size(){
     return size;
   }
 
+  public int size2(){
+    return size2;
+  }
+
   public String toString(){//ummmmm
-    if(size == 0) {
+    if(size2 == 0) {
       return "{}";
     }
     String str = "{";
     int i = start;
     int j = end;
     while(i!=j){
-      if(i>0&&i<size){
+      if(i>0&&i<size2){
         str+=data[i];
         i++;
       }
@@ -71,14 +76,15 @@ public class MyDeque<E>{
   }
 
   public void addFirst(E element){
-    /*if ((start == 0 && end == size-1)|| 
+    /*if ((start == 0 && end == size2-1)|| 
             start == end+1){
       resize(); //MAKE IT!!!
     }*/
     if (element == null){
       throw new NullPointerException();
     }
-    if(((start == 0 && end == size-1)|| 
+    
+    if(((start == 0 && end == size2-1)|| 
             start == end+1)){
       System.out.println("no");
       return;
@@ -87,16 +93,19 @@ public class MyDeque<E>{
       start = 0;
       end = 0;
       data[start] = element;
+      size++;
       return;
     }
     if(start == 0){
-      start = size-1;
+      start = size2-1;
       data[start] = element;
+      size++;
       return;
     }
     else{
       start--;
       data[start] = element;
+      size++;
       return;
     }
   }
@@ -105,7 +114,7 @@ public class MyDeque<E>{
     if (element == null){
       throw new NullPointerException();
     }
-    if(((start == 0 && end == size-1)|| 
+    if(((start == 0 && end == size2-1)|| 
             start == end+1)){
       System.out.println("no");
       return;
@@ -114,12 +123,14 @@ public class MyDeque<E>{
       start = 0;
       end = 0;
       data[start] = element;
+      size++;
       return;
     }
-    //if (end == size-1) ? end is last?
+    //if (end == size2-1) ? end is last?
     else{
       end++;
       data[start] = element;
+      size++;
       return;
     }
   }
@@ -131,12 +142,15 @@ public class MyDeque<E>{
     if(start == end){
       start = -1;
       end = -1;
+      size--;
     }
     else{
-      if(start == size-1){
+      if(start == size2-1){
         start = 0;
+        size--;
       }
       else{
+        size--;
         start++;
       }
     }
@@ -150,12 +164,15 @@ public class MyDeque<E>{
     if(start == end){
       start = -1;
       end = -1;
+      size--;
     }    
     else{
       if(end == 0){
-        end = size-1;
+        end = size2-1;
+        size--;
       }
       else{
+        size--;
         end--;
       }
     }
